23c17f1c3d6fdae92ac8edab13b0a3fd8c755e8b
GiBarsima <123210056+GiBarsima@users.noreply.github.com>
2023-02-28 23:00:48 -0300

    Delete javascript directory

--- javascript/tipo-de-variable.js
+++
@@ -1,8 +0,0 @@
-
-
-let nombre;     //definimos la variablex
-console.log(nombre);
-nombre = 5;      //X le asignamos un valor numerico
-console.log(nombre);
-let nombre = "John";     //X ahora almacena un string
-console.log(nombre);
